7a6b401ce805cad0504aa7775abf4373ebb44268
Alexey Marchuk <alexey@MacBook-Pro-Alexey.local>
2018-07-19 01:12:31 +0300

    Added new route

---
+++ src/Controller/Execute/ExecuteController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexey
+ * Date: 19.07.2018
+ * Time: 00:11
+ */
+
+namespace App\Controller\Execute;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * Class IndexController
+ * @package App\Controller
+ */
+class ExecuteController extends AbstractController
+{
+    /**
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     * @throws \Exception
+     */
+    public function execute(Request $request)
+    {
+
+        $code = $request->request;
+
+
+
+        return new JsonResponse($number);
+
+    }
+}
